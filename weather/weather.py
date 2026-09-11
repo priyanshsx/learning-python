5fdb3f65c301ac9a13517609e1c45d41c88045c8
Priyansh <saxenapriyansh95@gmail.com>
2026-09-11 19:03:33 +0530

    added the final script

--- weather/weather.py
+++ weather/weather.py
@@ -2,11 +2,21 @@ import requests
 from dotenv import load_dotenv
 import os 
 
-city = input("City you want to check the weather for: ").strip().lower()
+city = input("Enter the city you want weather for: ").strip().lower()
 
 load_dotenv()
-API_KEY=os.environ.get('WEATHER_API_KEY')
+API_KEY = os.environ.get('WEATHER_API_KEY')
 
 if not API_KEY:
-    raise RuntimeError("Weather API key not set. Please add it to the .env file in the root folder.")
+    print("Please enter your API key in a .env file in the project root folder.")
 
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    output = data['main']['temp']
+    print(f"{output} degree celsius")
+else:
+    print(f"Please enter a valid city name.")

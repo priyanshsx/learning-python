8ecf55358da16ce2969e6ce32bf5053f08c377da
Priyansh <saxenapriyansh95@gmail.com>
2026-09-11 15:35:29 +0530

    added the final script for the password generator

--- pwd_generator.py
+++ pwd_generator.py
@@ -1,12 +1,38 @@
-# ask the user for desired password length and ask if they want numbers, uppercase letters, or special symbols 
+import string 
+import secrets 
+
+# ask the user for input on desired password length and what all they would want in the pwd 
+# generator 
 
 length = int(input("Desired password length: ").strip())
-numbers = bool(input("Would you like numbers?: ").strip())
-casing = bool(input("Would you like your password to have both uppercase and lowercase?: ").strip())
-symbols = bool(input("And how about symbols?: ").strip())
+numbers = input("Would you like numbers? (y/n): ").strip().lower()
+casing = input("Would you like uppercase letters? (y/n): ").strip().lower()
+characters = input("Would you like special characters? (y/n): ").strip().lower()
 
-# create an empty string called char_pool 
+char_pool = string.ascii_lowercase
 
-# loop until a desired password length has been chosen 
+# checking for users' inputs and outputting the password accordingly 
 
-# display the password  
+if numbers == 'y':
+    char_pool += string.digits
+
+if casing == 'y': 
+    char_pool += string.ascii_uppercase
+
+if characters == 'y':
+    char_pool = string.punctuation
+
+# generating the password 
+
+password_list = []
+
+# creating a loop to generate the password 
+
+for _ in range(length):
+    random_char = secrets.choice(char_pool)
+    password_list.append(random_char)
+
+# display the password 
+
+final_password = ''.join(password_list)
+print(f"Your desired password is: {final_password}")

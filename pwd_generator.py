d5c7a427cce399d69883871a6a0b961ffa312a99
Priyansh <saxenapriyansh95@gmail.com>
2026-09-11 15:11:40 +0530

    added the pwd_generator.py file

---
+++ pwd_generator.py
@@ -0,0 +1,12 @@
+# ask the user for desired password length and ask if they want numbers, uppercase letters, or special symbols 
+
+length = int(input("Desired password length: ").strip())
+numbers = bool(input("Would you like numbers?: ").strip())
+casing = bool(input("Would you like your password to have both uppercase and lowercase?: ").strip())
+symbols = bool(input("And how about symbols?: ").strip())
+
+# create an empty string called char_pool 
+
+# loop until a desired password length has been chosen 
+
+# display the password  

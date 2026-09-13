f5213f5abf682483817ef654b16272dcb0a948e0
Priyansh <saxenapriyansh95@gmail.com>
2026-09-13 11:02:57 +0530

    updated the ohlcv downloader

---
+++ ohlcv_downloader/README.md
@@ -0,0 +1,4 @@
+# OHLCV Downloader from yfinance 
+
+
+NB: The db folder was just created to test out how data looks when pulled from yfinance. That is unprocessed data and of no use for any financial analysis. 
--- ohlcv_downloader/fetch.py
+++ ohlcv_downloader/fetch.py
@@ -3,7 +3,16 @@ import yfinance as yf
 # extracting data from yfinance by utilizing the given conditions 
 
 btc = yf.download('BTC-USD', start='2025-01-01', end='2026-01-01')
-print(btc.info())
-print("\n")
-print(btc.head(5))
+btc.to_csv('/home/priyansh/Documents/d/learning_python/ohlcv_downloader/db/btc_test.csv')
+# print(btc.info())
+# print("\n")
+# print(btc.head(5))
+# print(btc.index)
+print(btc.columns)
+
+# using the above, we find that there are 6 columns in total titled: 
+# Price, Close, High, Low, Open, Volume 
+# further: the top 2 rows are non-functional as they just have BTC-USD values repeated all over 
+# with the exception of the price column which is actually not price but the date 
+# however it is already the datetimeindex 
 

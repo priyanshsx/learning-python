682e2b0d52563a57b0d94df6e4efb21b88ccf0ba
Priyansh <saxenapriyansh95@gmail.com>
2026-09-13 09:48:09 +0530

    updated the ohlcv fetch script

--- ohlcv_downloader/fetch.py
+++ ohlcv_downloader/fetch.py
@@ -1,4 +1,9 @@
 import yfinance as yf
 
-intc = yf.download('', start='2025-01-01', end='2026-01-01')
-intc.to_csv('/test_intc.csv')
+# extracting data from yfinance by utilizing the given conditions 
+
+btc = yf.download('BTC-USD', start='2025-01-01', end='2026-01-01')
+print(btc.info())
+print("\n")
+print(btc.head(5))
+
